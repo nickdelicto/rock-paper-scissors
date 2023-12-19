72992c7792b312efd8f7022827b7d24b70fb0ffb
Nicholas Githinji <113605917+nickdelicto@users.noreply.github.com>
2023-12-19 22:57:10 +0300

    Add event listeners to UI buttons
    Modified game logic to allow clicking buttons to play.

--- script.js
+++ script.js
@@ -1,70 +1,75 @@
-let rock = "rock";
-let paper = "paper";
-let scissors = "scissors";
+const rockButton = document.querySelector('.rock');
+rockButton.value = "rock";
+const paperButton = document.querySelector('.paper');
+paperButton.value = "paper";
+const scissorsButton = document.querySelector('.scissors');
+scissorsButton.value = "scissors";
 
-/* Declare a function "getComputerChoice" to randomly return either,
-'Rock', 'Paper' or 'Scissors' */
+
+rockButton.addEventListener('click', (e) => {
+    playRound(e.target.value, getComputerChoice())
+});
+
+paperButton.addEventListener('click', (e) => {
+    playRound(e.target.value, getComputerChoice())
+});
+
+scissorsButton.addEventListener('click', (e) => {
+    playRound(e.target.value, getComputerChoice())
+});
+
+
 function getComputerChoice() {
-let array = [rock, paper, scissors];
-let random = (Math.floor(Math.random() * array.length));
-return array[random];
+    let array = [rockButton.value, paperButton.value, scissorsButton.value];
+    let random = (Math.floor(Math.random() * array.length));
+    return array[random];
 }
 
 let computerSelection;
 let playerSelection;
 
-/* Define function taking the user input and computer's random choice,
-as arguments.*/
+const body = document.querySelector('body');
+const display = document.createElement('div');
+body.appendChild(display);
+
+let finalPlayerScore = 0;
+let finalComputerScore = 0;
+
 function playRound(playerSelection, computerSelection) {
-    do {
-        playerSelection = prompt("Rock, Paper, Scissors, shoot!").toLowerCase();
-    } while (playerSelection !== rock && playerSelection !== paper && playerSelection !== scissors);
-
-    computerSelection = getComputerChoice();
-
-    if ((playerSelection === rock) && (computerSelection === scissors)) {
-        return "player";
-    } else if ((playerSelection === scissors) && (computerSelection === paper)) {
-        return "player";
-    } else if ((playerSelection === paper) && (computerSelection === rock)) {
-        return "player";
-    } else if ((playerSelection === rock) && (computerSelection === paper)) {
-        return "computer";
-    } else if ((playerSelection === paper) && (computerSelection === scissors)) {
-        return "computer";
-    } else if ((playerSelection === scissors) && (computerSelection === rock)) {
-        return "computer";
+    if ((playerSelection === rockButton.value) && (computerSelection === scissorsButton.value)) {
+        finalPlayerScore++
+        display.textContent = `You win this round. Player: ${finalPlayerScore} vs. Computer: ${finalComputerScore}`;
+    } else if ((playerSelection === scissorsButton.value) && (computerSelection === paperButton.value)) {
+        finalPlayerScore++
+        display.textContent = `You win this round. Player: ${finalPlayerScore} vs. Computer: ${finalComputerScore}`;
+    } else if ((playerSelection === paperButton.value) && (computerSelection === rockButton.value)) {
+        finalPlayerScore++
+        display.textContent = `You win this round. Player: ${finalPlayerScore} vs. Computer: ${finalComputerScore}`;
+    } else if ((playerSelection === rockButton.value) && (computerSelection === paperButton.value)) {
+        finalComputerScore++
+        display.textContent = `You lose this round. Player: ${finalPlayerScore} vs. Computer: ${finalComputerScore}`;
+    } else if ((playerSelection === paperButton.value) && (computerSelection === scissorsButton.value)) {
+        finalComputerScore++
+        display.textContent = `You lose this round. Player: ${finalPlayerScore} vs. Computer: ${finalComputerScore}`;
+    } else if ((playerSelection === scissorsButton.value) && (computerSelection === rockButton.value)) {
+        finalComputerScore++
+        display.textContent = `You lose this round. Player: ${finalPlayerScore} vs. Computer: ${finalComputerScore}`;
     } else if (playerSelection === computerSelection) {
-        return "tie";
+        display.textContent = `You tie this round. Player: ${finalPlayerScore} vs. Computer: ${finalComputerScore}`;
+    }
+
+    if (finalPlayerScore === 5 || finalComputerScore === 5) {
+        if (finalPlayerScore > finalComputerScore) {
+            display.textContent = `Congrats! You've won the game! 
+            Player: ${finalPlayerScore} vs. Computer: ${finalComputerScore}`;
+        } else {
+            display.textContent = `Sorry! You've lost the game! 
+            Player: ${finalPlayerScore} vs. Computer: ${finalComputerScore}`; 
+        }
+
+        finalPlayerScore = 0;
+        finalComputerScore = 0;
     }
 }
 
-/* Define a function game(). Do a loop calling the playRound function
-5 times. For each loop, tally finalPlayerScore and finalComputerScore*/
-function game() {
-    let finalPlayerScore = 0
-    let finalComputerScore = 0
-
-    for (let i = 1; i <= 5; i++) {
-        let roundResult = playRound(playerSelection, computerSelection)
-
-        if (roundResult === "player") {
-            finalPlayerScore++;
-            console.log(`You win in round ${i}!`);
-        } else if (roundResult === "computer") {
-            finalComputerScore++;
-            console.log(`You lose in round ${i}!`);
-        } else if (roundResult === "tie") {
-            console.log(`You tie in round ${i}!`);
-        }
-    }
-    
-    if (finalPlayerScore > finalComputerScore) {
-        console.log(`You Win! Your score: ${finalPlayerScore} / 5. Computer score: ${finalComputerScore} / 5.`);
-    } else if (finalPlayerScore < finalComputerScore) {
-        console.log(`You Lose! Your score: ${finalPlayerScore} / 5. Computer score: ${finalComputerScore} / 5.`);
-    } else if (finalPlayerScore === finalComputerScore) {
-        console.log(`It's a Tie! Your score: ${finalPlayerScore} / 5. Computer score: ${finalComputerScore} / 5.`)
-    }
-} console.log(game());
 
